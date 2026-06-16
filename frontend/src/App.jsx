ac01e3479868483d33027d55cae6d4e3507045c1
Daniel Ruz <Dani.Ruzanov@gmail.com>
2026-06-16 15:21:27 +0300

    typo fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,19 @@ import { useState } from 'react'
 
 import { Provider } from './components/ui/provider'
 import { BrowserRouter as Roter, Routes, Route, BrowserRouter } from 'react-router-dom'
-import { ButtonProvider } from './context/ButtonContext'
+import { SignProvider } from './context/SignContext'
 
 import Home from './pages/Home'
 
 function App() {
-
   return (
     <Provider>
       <BrowserRouter>
-        <ButtonProvider>
+        <SignProvider>
           <Routes>
             <Route path='/' element={<Home />} />
           </Routes>
-        </ButtonProvider>
+        </SignProvider>
       </BrowserRouter>
     </Provider>
   )
