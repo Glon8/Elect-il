bed1195122a2fb84026b37f902edd5c92a15cd2f
Daniel Ruz <Dani.Ruzanov@gmail.com>
2026-08-04 16:10:53 +0300

    added custom translation packages

--- frontend/src/context/LanguageContext.jsx
+++ frontend/src/context/LanguageContext.jsx
@@ -1,27 +1,29 @@
 import React, { createContext, useEffect, useState } from 'react'
+import { content } from '../translationPackages/elect_il_en'
 
 export const LanguageContext = createContext(null);
 
 export const LanguageProvider = ({ children }) => {
-    const [useLang, setLang] = useState('he');
+    const [useLang, setLang] = useState('eng');
+    const [useTrans, setTrans] = useState(content);
     const [useOrien, setOrien] = useState('rtl');
 
     useEffect(() => {
         if (!['ru', 'eng', 'he'].includes(useLang)) return;
 
         useLang == 'he' ? setOrien('rtl') : setOrien('ltr');
-        // pull the language from the server - extract translation file
+        // pull the language from the server - extract translation file ?? if no extraction file, use default of "content from elect_il_en"/*  */
     }, [useLang]);
 
     useEffect(() => {
         // step 1: check the link for language param - find the language attempt 1
         // step 2: get the browser language - find the language attempt 2
-        // step 3: pull the language from the server - extract translation file
+        // step 3: pull the language from the server - extract translation file ?? if no extraction file, use default of "content from elect_il_en"
     }, []);
 
     return (
         <LanguageContext.Provider
-            value={{ language: useLang, setLanguage: setLang, orientation: useOrien }}>
+            value={{ language: useLang, setLanguage: setLang, translation: useTrans, orientation: useOrien }}>
             {children}
         </LanguageContext.Provider>
     );
