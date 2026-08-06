02f0327c1a85a09d2ec487af30b105d197cfb567
Daniel Ruz <Dani.Ruzanov@gmail.com>
2026-08-06 17:08:49 +0300

    new language selection UI linked to languageContext

--- frontend/src/components/LangSelect.jsx
+++ frontend/src/components/LangSelect.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 import './css/LangSelect.css';
 
+import { LanguageContext } from '../context/LanguageContext';
+
 export default function LangSelect({ list }) {
+    const { language, langUpdate } = useContext(LanguageContext);
+
     return (
-        <select className='def' defaultValue={'he'}>
+        <select className='def' value={language} onChange={(e) => langUpdate(e.target.value)}>
             {
                 list.map((item, ind) => {
-                    return (<option key={`lang${ind}`} value={item.toLowerCase()}>{item}</option>);
+                    return (<option disabled={language == item.toLowerCase()} key={`lang${ind}`} value={item.toLowerCase()}>{item}</option>);
                 })
             }
         </select>
