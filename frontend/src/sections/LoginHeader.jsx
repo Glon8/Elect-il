7145524431a0c4454a0fe295f8a7db0f9827bdcf
Daniel Ruz <Dani.Ruzanov@gmail.com>
2026-08-07 15:10:48 +0300

    adjusted login header text for better clearity on mobile

--- frontend/src/sections/LoginHeader.jsx
+++ frontend/src/sections/LoginHeader.jsx
@@ -10,8 +10,8 @@ export default function LoginHeader({ ...props }) {
     return (
         <HeadBody display={!isSmall ? 'none' : 'flex'} background={'white/80'} top={'4rem'} h={'2rem'} zIndex={1000} borderBottomStyle={'solid'} borderBottomWidth={1} borderBottomColor={'border.emphasized/40'} justifyContent={'space-between'} {...props} >
             <Flex w={'full'} flexDir={'row'} gapX={5}>
-                <Text w={'50%'} color={'gray.400/70'} fontWeight={'medium'} alignContent={'center'} fontSize={'md'} textAlign={'center'}>Vasiliy Pupkin</Text>
-                <Text w={'50%'} color={'gray.400/70'} fontWeight={'medium'} alignContent={'center'} fontSize={'md'} textAlign={'center'}>7654321</Text>
+                <Text w={'50%'} color={'gray.400'} fontWeight={'medium'} alignContent={'center'} fontSize={'md'} textAlign={'center'}>Vasiliy Pupkin</Text>
+                <Text w={'50%'} color={'gray.400'} fontWeight={'medium'} alignContent={'center'} fontSize={'md'} textAlign={'center'}>7654321</Text>
             </Flex>
         </HeadBody >
     )
